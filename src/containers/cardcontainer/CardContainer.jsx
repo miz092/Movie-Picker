d37bb2bbde20f20108039c64b1ef3ba98ed3c3db
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-27 17:48:07 +0100

    init card moviecardcontainer and moviepage

--- src/containers/cardcontainer/CardContainer.jsx
+++ src/containers/cardcontainer/CardContainer.jsx
@@ -1,4 +1,5 @@
 import MovieCard from "../../components/moviecard/MovieCard.jsx";
+import ErrorPage from "../../components/errorpage/ErrorPage.jsx";
 import "./CardContainer.css";
 
 export default function CardContainer({ movies }) {
